26931afd4d83d8e546f904612c0c19033c2359f3
Delhommer, Rick <rdelhommer@illumina.com>
2016-08-11 10:56:19 -0700

    some slight changes to com0com stdout regex parsing. Also return the created CrossoverPortPair from CreatePortPair()

--- src/Com0Com.CSharp/Com0ComSetupCFacade.cs
+++ src/Com0Com.CSharp/Com0ComSetupCFacade.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
-using System.Windows.Forms;
 
 namespace Com0Com.CSharp
 {
-	public static class Com0comSetup
+	public static class Com0ComSetupCFacade
 	{
 		private readonly static string _com0comSetupc = @"C:\Program Files (x86)\com0com\setupc.exe";
 
@@ -17,14 +17,12 @@ namespace Com0Com.CSharp
 		/// <returns></returns>
 		public static IEnumerable<CrossoverPortPair> GetCrossoverPortPairs()
 		{
-			var ports = new List<CrossoverPortPair>();
-			//get the output from setupc --detail-prms list
 			var proc = new Process
 			{
 				StartInfo = new ProcessStartInfo
 				{
 					FileName = _com0comSetupc,
-					Arguments = "--detail-prms list",
+					Arguments = "list",
 					UseShellExecute = false,
 					RedirectStandardOutput = true,
 					CreateNoWindow = true
@@ -32,40 +30,17 @@ namespace Com0Com.CSharp
 			};
 
 			proc.Start();
+
+			var lines = new List<string>();
 			while (!proc.StandardOutput.EndOfStream)
 			{
-				try
-				{
-					var portNameA = "";
-					var portNameB = "";
-						
-					string line = proc.StandardOutput.ReadLine();
-					//get port number
-					Regex regex = new Regex(@"(?<=CNC[A,B])\d+(?=\s)");
-					int portnum = int.Parse(regex.Match(line).Value);
-
-					regex = new Regex(@"(?<=CNC)[A,B](?=\d+\s)");
-					string portAB = regex.Match(line).Value;
-					if (portAB == "A")
-					{
-						portNameA = line;
-					}
-					else if (portAB == "B")
-					{
-						portNameB = line;
-					}
-
-					ports.Add(new CrossoverPortPair(portNameA, portNameB, portnum));
-				}
-				catch (Exception ex)
-				{
-					MessageBox.Show(ex.ToString());
-				}
+				lines.Add(proc.StandardOutput.ReadLine());
 			}
-			return ports;
+
+			return ParsePortPairsFromStdOut(lines);
 		}
 
-		public static void CreatePortPair()
+		public static CrossoverPortPair CreatePortPair(string comPortNameA, string comPortNameB)
 		{
 			if (UacHelper.IsUacEnabled && !UacHelper.IsProcessElevated
 			    || !UacHelper.IsAdministrator())
@@ -77,7 +52,7 @@ namespace Com0Com.CSharp
 				{
 					WorkingDirectory = Path.GetDirectoryName(_com0comSetupc),
 					FileName = _com0comSetupc,
-					Arguments = "install - -",
+					Arguments = $"install portname={comPortNameA} portname={comPortNameB}",
 					UseShellExecute = true,
 					CreateNoWindow = false,
 					Verb = "runas"
@@ -85,11 +60,13 @@ namespace Com0Com.CSharp
 			};
 			proc.Start();
 
-			//TODO: add a timeout here
-			while (!proc.HasExited) { }
+			var lines = new List<string>();
+			while (!proc.StandardOutput.EndOfStream)
+			{
+				lines.Add(proc.StandardOutput.ReadLine());
+			}
 
-			if (proc.ExitCode != 0)
-				throw new ApplicationException($"Could not add port pair.");
+			return ParsePortPairsFromStdOut(lines).First();
 		}
 
 		public static void DeletePortPair(int n)
@@ -112,12 +89,46 @@ namespace Com0Com.CSharp
 			};
 			proc.Start();
 
-			//TODO: add a timeout here
 			while (!proc.HasExited) { }
 
 			if (proc.ExitCode != 0)
 				throw new ApplicationException($"Could not delete port pair number {n}");
 		}
+
+		private static IEnumerable<CrossoverPortPair> ParsePortPairsFromStdOut(IEnumerable<string> lines)
+		{
+			var portAMap = new Dictionary<int, string>();
+			var portBMap = new Dictionary<int, string>();
+
+			foreach (var line in lines)
+			{
+				var lineRegex = new Regex(@"^CNC([A,B])(\d+)\sPortName=(\w+)");
+				var match = lineRegex.Match(line);
+				if (match == null) continue;
+
+				var portNum = Convert.ToInt32(match.Groups[1].Value);
+				var aOrB = match.Groups[0].Value;
+				var portName = match.Groups[2].Value;
+
+				if (aOrB == "A")
+				{
+					portAMap.Add(portNum, portName);
+				}
+				else
+				{
+					portBMap.Add(portNum, portName);
+				}
+			}
+
+			var ret = new List<CrossoverPortPair>();
+			for (var i = 0; i < portAMap.Keys.Count; i++)
+			{
+				ret.Add(new CrossoverPortPair(portBMap[i], portBMap[i], i));
+			}
+
+			return ret;
+		}
 	}
+
 }
 
