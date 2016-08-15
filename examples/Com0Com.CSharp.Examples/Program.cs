7c63f91807d3f56eec491a1cf7205a3bef9e31f3
rdelhommer <rdelhommer@illumina.com>
2016-08-15 16:46:18 -0700

    remove old wpf project

--- examples/Com0Com.CSharp.Examples/Program.cs
+++ examples/Com0Com.CSharp.Examples/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Com0Com.CSharp.Examples
+﻿namespace Com0Com.CSharp.Examples
 {
     class Program
     {
