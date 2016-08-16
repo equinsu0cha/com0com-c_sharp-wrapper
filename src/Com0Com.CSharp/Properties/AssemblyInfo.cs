07172cc38376e9ecabd7518a9813639a171dc144
rdelhommer <rdelhommer@illumina.com>
2016-08-15 17:44:16 -0700

    Update AssemblyInfo for nuget pack

--- src/Com0Com.CSharp/Properties/AssemblyInfo.cs
+++ src/Com0Com.CSharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Com0Com.CSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# wrapper for the com0com null-modem emulator.  To use this you must have installed com0com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rick Delhommer")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("rdelhommer")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
