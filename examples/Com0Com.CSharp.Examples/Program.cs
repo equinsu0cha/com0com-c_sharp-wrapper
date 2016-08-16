fe0c6475c0b4ceb7e1d7a5be9e637bda6402549b
rdelhommer <rdelhommer@illumina.com>
2016-08-15 17:22:47 -0700

    Make example more readable

--- examples/Com0Com.CSharp.Examples/Program.cs
+++ examples/Com0Com.CSharp.Examples/Program.cs
@@ -9,27 +9,31 @@ namespace Com0Com.CSharp.Examples
 
         static void Main(string[] args)
         {
-            // Get all the pre existing virtual com port pairs
+            Console.WriteLine("Pre-existing virtual crossover port pairs:");
             var preExistingPortPairs = SetupCFacade.GetCrossoverPortPairs();
             foreach (var pp in preExistingPortPairs)
             {
                 Console.WriteLine($"Virtual Port Pair: CNCA{pp.PairNumber}({pp.PortNameA}) <-> CNCB{pp.PairNumber}({pp.PortNameB})");
             }
+            Console.WriteLine();
 
             // Create some new virtual com port pairs
             var pp1 = SetupCFacade.CreatePortPair();
             var pp2 = SetupCFacade.CreatePortPair("COM180", "COM181");
 
+            Console.WriteLine("Virtual crossover port pairs after creation:");
             var portPairsAfterCreation = SetupCFacade.GetCrossoverPortPairs();
             foreach (var pp in portPairsAfterCreation)
             {
                 Console.WriteLine($"Virtual Port Pair: CNCA{pp.PairNumber}({pp.PortNameA}) <-> CNCB{pp.PairNumber}({pp.PortNameB})");
             }
+            Console.WriteLine();
 
             // Remove the virtual com port pairs that we created
             SetupCFacade.DeletePortPair(pp1.PairNumber);
             SetupCFacade.DeletePortPair(pp2.PairNumber);
 
+            Console.WriteLine("Virtual crossover port pairs after removal:");
             var portPairsAfterDelete = SetupCFacade.GetCrossoverPortPairs();
             foreach (var pp in portPairsAfterDelete)
             {
